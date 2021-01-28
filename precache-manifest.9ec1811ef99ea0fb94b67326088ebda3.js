340be180b5bb301a1715aa4d65f5dbc06eeda022
Pablo Gómez Ponce <pgomezponce@gmail.com>
2021-01-28 11:59:31 +0100

    Updates

---
+++ precache-manifest.9ec1811ef99ea0fb94b67326088ebda3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebb0e2cae31977dc8cbc",
+    "url": "/blobs/static/js/app.a0e67574.chunk.js"
+  },
+  {
+    "revision": "fe45875893f3375b1178",
+    "url": "/blobs/static/js/runtime~app.86e2590e.js"
+  },
+  {
+    "revision": "41179b917c21e540e1ae",
+    "url": "/blobs/static/js/2.022dde17.chunk.js"
+  },
+  {
+    "revision": "1ac1a43f0a97f93058b8846525bcb619",
+    "url": "/blobs/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/blobs/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/blobs/expo-service-worker.js"
+  },
+  {
+    "revision": "b27a845d510f00d5662c1ac4a210fa91",
+    "url": "/blobs/register-service-worker.js"
+  },
+  {
+    "revision": "aff0749df7ab2f0f309f5be9bc3a5356",
+    "url": "/blobs/static/js/2.022dde17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "521f8c844d3af13c3d20ba01b2fc5274",
+    "url": "/blobs/favicon-16.png"
+  },
+  {
+    "revision": "d50b3a5941cfe755e600a75113771581",
+    "url": "/blobs/favicon-32.png"
+  },
+  {
+    "revision": "fa6155203a3b9f3590820927f5a5991a",
+    "url": "/blobs/favicon.ico"
+  },
+  {
+    "revision": "04f17f7741d19b2b3bd81f7b42d03b88",
+    "url": "/blobs/manifest.json"
+  }
+];
---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/blobs/expo-service-worker.js",{scope:"/blobs/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
